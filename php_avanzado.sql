ca11418b64ef3914d9604a4a6799890e947f547c
Alejandro Daniel Di Stefano <addistefano76@gmail.com>
2022-10-02 19:52:47 -0300

    02

---
+++ php_avanzado.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-10-2022 a las 00:51:43
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `php_avanzado`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id_cliente` int(11) NOT NULL,
+  `nombre_c` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `apellido_c` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `telefono_c` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `email_c` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `mensaje_c` text COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id_cliente`, `nombre_c`, `apellido_c`, `telefono_c`, `email_c`, `mensaje_c`) VALUES
+(1, 'Alejandro', 'Di Stefano', '1140005555', 'alejandro@mail.com', 'Hola'),
+(2, 'Alejandro', 'Di Stefano', '1140005555', 'alejandro@mail.com', 'Hola');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registros`
+--
+
+CREATE TABLE `registros` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(200) COLLATE utf32_spanish2_ci NOT NULL,
+  `email` varchar(200) COLLATE utf32_spanish2_ci NOT NULL,
+  `pass` varchar(100) COLLATE utf32_spanish2_ci NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `registros`
+--
+
+INSERT INTO `registros` (`id`, `nombre`, `email`, `pass`, `fecha`) VALUES
+(1, 'Alejandro Daniel', 'alejandro@mail.com', '123', '2022-09-26 21:03:19'),
+(10, 'Fernando Palma', 'fpalma@mail.com', 'cc882994945ac7e3528bc7887a66db3e', '2022-09-29 01:55:12');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Indices de la tabla `registros`
+--
+ALTER TABLE `registros`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `registros`
+--
+ALTER TABLE `registros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
